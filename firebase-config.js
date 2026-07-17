dd8c9964c74474860aed0ae9b8e6ef3184290ddc
akvsak1997-svg <akvsak1997@gmail.com>
2026-07-17 17:53:50 +0600

    Add Firebase configuration to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZHoWfFFbEJ5lbCxNuepS84MMdrP7X3Ck",
+  authDomain: "adiyan-live-tv.firebaseapp.com",
+  databaseURL: "https://adiyan-live-tv-default-rtdb.firebaseio.com",
+  projectId: "adiyan-live-tv",
+  storageBucket: "adiyan-live-tv.firebasestorage.app",
+  messagingSenderId: "374397306957",
+  appId: "1:374397306957:web:ab130d93cf02d4dad2f404"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
